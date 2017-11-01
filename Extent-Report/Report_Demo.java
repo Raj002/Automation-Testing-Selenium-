e02bf1395a62333e97a9674b4cdfee56e0433cb6
Raj002 <rajava015@gmail.com>
2017-11-01 10:34:56 -0400

    Update Report_Demo.java

--- Extent-Report/Report_Demo.java
+++ Extent-Report/Report_Demo.java
@@ -141,3 +141,111 @@ public class Report_Demo
 	}
 }
 
+
+
+
+//################################## pom.xml #################################
+<dependency>
+	<groupId>com.relevantcodes</groupId>
+	<artifactId>extentreports</artifactId>
+	version>2.41.2</version>
+</dependency>
+<dependency>
+	<groupId>org.freemarker</groupId>
+	<artifactId>freemarker</artifactId>
+	<version>2.3.23</version>
+</dependency>
+
+//####################################### testng.xml #################################
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE suite SYSTEM "http://testng.org/testng-1.0.dtd">
+<suite name="Suite">
+
+  <listeners>
+  	<listener class-name="com.TEA.Framework.Claims.ExtentReportListener.ExtentReportListener"></listener>
+  </listeners>
+  
+  <test name="Test">
+    <classes>
+      <class name="com.TEA.Framework.RiskTrac.JBehave.StoryConfig"/>
+    </classes>
+  </test> <!-- Test -->
+</suite> <!-- Suite -->
+	      
+//######################################## ExtentReportListener.java ###################################
+package com.TEA.Framework.Claims.ExtentReportListener;
+
+import java.io.File;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.testng.IReporter;
+import org.testng.IResultMap;
+import org.testng.ISuite;
+import org.testng.ISuiteResult;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.xml.XmlSuite;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+public class ExtentReportListener implements IReporter {
+	private ExtentReports extent;
+
+	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites,
+			String outputDirectory) {
+		extent = new ExtentReports(outputDirectory + File.separator
+				+ "Extent.html", true);
+
+		for (ISuite suite : suites) {
+			Map<String, ISuiteResult> result = suite.getResults();
+
+			for (ISuiteResult r : result.values()) {
+				ITestContext context = r.getTestContext();
+
+				buildTestNodes(context.getPassedTests(), LogStatus.PASS);
+				buildTestNodes(context.getFailedTests(), LogStatus.FAIL);
+				buildTestNodes(context.getSkippedTests(), LogStatus.SKIP);
+			}
+		}
+
+		extent.flush();
+		extent.close();
+	}
+
+	private void buildTestNodes(IResultMap tests, LogStatus status) {
+		ExtentTest test;
+
+		if (tests.size() > 0) {
+			for (ITestResult result : tests.getAllResults()) {
+				test = extent.startTest(result.getMethod().getMethodName());
+
+				test.setStartedTime(getTime(result.getStartMillis()));
+				test.setEndedTime(getTime(result.getEndMillis()));
+
+				for (String group : result.getMethod().getGroups())
+					test.assignCategory(group);
+
+				if (result.getThrowable() != null) {
+					test.log(status, result.getThrowable());
+				} else {
+					test.log(status, "Test " + status.toString().toLowerCase()
+							+ "ed");
+				}
+
+				extent.endTest(test);
+			}
+		}
+	}
+
+	private Date getTime(long millis) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(millis);
+		return calendar.getTime();
+	}
+	
+}
